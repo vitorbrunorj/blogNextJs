2f1f774f6b0fca2bd7f1273cc9856a0494228449
Vitor Bruno <vitorbrunorj@gmail.com>
2023-11-21 16:38:01 -0300

    Configurando o projeto

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Pagina 404</h2>
+      <p>configurar a pagina</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  )
+}
