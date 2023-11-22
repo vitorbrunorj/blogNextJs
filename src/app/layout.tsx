a0403cea1081547125a2aac38032b9451446ff7d
Vitor Bruno <vitorbrunorj@gmail.com>
2023-11-21 23:39:32 -0300

    Configurando o projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
+/* eslint-disable @typescript-eslint/member-delimiter-style */
 import './globals.css'
 import Head from 'next/head'
 import { Header } from '@/components/header'
 import { Footer } from '@/components/footer'
+import Favicon from '@/public/favicon.ico'
 
 interface Metadata {
   title: string
   description: string
   keywords: string[]
   metadataBase: string
+  icons: Array<{ rel: string; url: string }>
   openGraph: {
     title: string
     images: string[]
@@ -28,6 +31,7 @@ export const metadata: Metadata = {
   title: 'Meu Site - Aprendendo NextJS',
   description: 'Site completo para praticar nextjs com sujeito programador',
   keywords: ['HTML', 'CSS', 'JavaScript', 'Programação'],
+  icons: [{ rel: 'icon', url: Favicon.src }],
   metadataBase: 'https://meusite.com',
   openGraph: {
     title: 'Aprendendo Next JS com Sujeito',
@@ -55,7 +59,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br" suppressHydrationWarning={true}>
       <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/src/public/favicon.ico" />
       </Head>
       <body suppressHydrationWarning={true}>
         <Header />
